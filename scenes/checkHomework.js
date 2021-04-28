fc88a5875ccbe7a0035d328dc3105a76511f49b9
DamirAhm <damir221003@gmail.com>
2021-04-28 14:37:10 +0400

    Fixed checkHomework scene (previously sent homework even student wasn't in onlyFor list)

--- scenes/checkHomework.js
+++ scenes/checkHomework.js
@@ -1,26 +1,33 @@
 //@ts-check
 
-const config = require('../config.js');
-const { buttonColors, sceneNames } = require('../utils/constants.js');
-const { getDayMonthString, getTomorrowDate, parseDate } = require('../utils/dateFunctions.js');
-const Scene = require('node-vk-bot-api/lib/scene'),
+const config = require("../config.js");
+const { buttonColors, sceneNames } = require("../utils/constants.js");
+const {
+	getDayMonthString,
+	getTomorrowDate,
+	parseDate,
+} = require("../utils/dateFunctions.js");
+const Scene = require("node-vk-bot-api/lib/scene"),
 	{
 		createDefaultKeyboard,
 		createBackKeyboard,
 		monthsRP,
-	} = require('../utils/messagePayloading.js'),
-	{ DataBase: DB } = require('bot-database'),
-	{ mapHomeworkByLesson, filterContentByDate } = require('bot-database/build/utils/functions'),
-	botCommands = require('../utils/botCommands.js'),
-	Markup = require('node-vk-bot-api/lib/markup'),
+	} = require("../utils/messagePayloading.js"),
+	{ DataBase: DB } = require("bot-database"),
+	{
+		mapHomeworkByLesson,
+		filterContentByDate,
+	} = require("bot-database/build/utils/functions"),
+	botCommands = require("../utils/botCommands.js"),
+	Markup = require("node-vk-bot-api/lib/markup"),
 	DataBase = new DB(process.env.MONGODB_URI),
-	{ isAdmin } = require('../utils/roleChecks.js'),
-	{ getHomeworkPayload } = require('../utils/studentsNotification'),
-	{ getLengthOfHomeworkWeek, mapButtons } = require('../utils/functions'),
-	{ isValidDateString } = require('../utils/validators'),
-	{ sendHomework } = require('../utils/studentsNotification'),
-	{ cleanDataForSceneFromSession } = require('../utils/sessionCleaners'),
-	{ pickSchoolAndClassAction } = require('../utils/actions');
+	{ isAdmin } = require("../utils/roleChecks.js"),
+	{ getHomeworkPayload } = require("../utils/studentsNotification"),
+	{ getLengthOfHomeworkWeek, mapButtons } = require("../utils/functions"),
+	{ isValidDateString } = require("../utils/validators"),
+	{ sendHomework } = require("../utils/studentsNotification"),
+	{ cleanDataForSceneFromSession } = require("../utils/sessionCleaners"),
+	{ pickSchoolAndClassAction } = require("../utils/actions");
 
 const { isNeedToPickClass } = config;
 
@@ -38,22 +45,25 @@ const checkHomeworkScene = new Scene(
 			const needToPickClass = (await isAdmin(ctx)) && isNeedToPickClass;
 			if (needToPickClass && !ctx.session.Class) {
 				ctx.session.nextScene = sceneNames.checkHomework;
-				ctx.session.pickFor = 'Выберите класс у которого хотите посмотреть дз \n';
+				ctx.session.pickFor =
+					"Выберите класс у которого хотите посмотреть дз \n";
 				ctx.scene.enter(sceneNames.pickClass);
 			} else {
 				const Student = await DataBase.getStudentByVkId(
-					ctx.session.userId || ctx.message.user_id,
+					ctx.session.userId || ctx.message.user_id
 				);
 				if (Student) {
 					if (Student.registered) {
 						if (Student.class === null) {
 							ctx.reply(
-								'Для использования данной функции необходимо войти в класс, для начала введите номер своей школы',
+								"Для использования данной функции необходимо войти в класс, для начала введите номер своей школы",
 								null,
-								createBackKeyboard([[Markup.button(botCommands.checkExisting)]]),
+								createBackKeyboard([[Markup.button(botCommands.checkExisting)]])
 							);
 
-							pickSchoolAndClassAction(ctx, { nextScene: sceneNames.checkHomework });
+							pickSchoolAndClassAction(ctx, {
+								nextScene: sceneNames.checkHomework,
+							});
 							return;
 						}
 
@@ -62,24 +72,29 @@ const checkHomeworkScene = new Scene(
 
 						ctx.scene.next();
 						ctx.reply(
-							'На какую дату вы хотите узнать задание? (в формате ДД.ММ)',
+							"На какую дату вы хотите узнать задание? (в формате ДД.ММ)",
 							null,
 							createBackKeyboard(
 								mapButtons([
-									[Markup.button(botCommands.onTomorrow, buttonColors.positive)],
+									[
+										Markup.button(
+											botCommands.onTomorrow,
+											buttonColors.positive
+										),
+									],
 									[
 										new Date().getDay() >= 5,
 										[Markup.button(botCommands.nextWeek, buttonColors.primary)],
 										[Markup.button(botCommands.thisWeek, buttonColors.primary)],
 									],
-								]),
-							),
+								])
+							)
 						);
 					} else {
 						ctx.scene.enter(sceneNames.register);
 					}
 				} else {
-					throw new Error('Student is not exists');
+					throw new Error("Student is not exists");
 				}
 			}
 		} catch (e) {
@@ -126,7 +141,7 @@ const checkHomeworkScene = new Scene(
 						const dateItMilliseconds = new Date(
 							today.getFullYear(),
 							today.getMonth(),
-							dayOfHomework,
+							dayOfHomework
 						);
 						const date = new Date(dateItMilliseconds);
 
@@ -137,10 +152,15 @@ const checkHomeworkScene = new Scene(
 								classNameOrInstance: ctx.session.Class,
 								schoolName: ctx.session.Class.schoolName,
 							},
-							date,
+							date
+						);
+						//exclusive in this case is homework with ids in onlyFor prop
+						const homeworkWithoutExclusives = homework.filter(
+							({ onlyFor }) =>
+								onlyFor.length === 0 || onlyFor.includes(ctx.message.user_id)
 						);
 
-						if (homework.length === 0) {
+						if (homeworkWithoutExclusives.length === 0) {
 							//? IIFE to make amountOfHomework local closure elsewhere it would be saved as valiable at moment when setTimeout callback will be executed
 							((delayAmount) =>
 								setTimeout(() => {
@@ -148,7 +168,9 @@ const checkHomeworkScene = new Scene(
 								}, messageDelay * delayAmount))(delayAmount);
 							delayAmount++;
 						} else {
-							const parsedHomework = mapHomeworkByLesson(homework);
+							const parsedHomework = mapHomeworkByLesson(
+								homeworkWithoutExclusives
+							);
 
 							let headerMessage = `Задание на ${dateString}\n`;
 
@@ -164,7 +186,7 @@ const checkHomeworkScene = new Scene(
 							for (const [lesson, homework] of parsedHomework) {
 								const { homeworkMessage, attachments } = getHomeworkPayload(
 									lesson,
-									homework,
+									homework
 								);
 
 								setTimeout(() => {
@@ -198,22 +220,31 @@ const checkHomeworkScene = new Scene(
 
 					date = new Date(year, month - 1, day);
 				} else {
-					ctx.reply('Дата должна быть в формате ДД.ММ');
+					ctx.reply("Дата должна быть в формате ДД.ММ");
 					return;
 				}
 
 				if (date) {
-					const homework = filterContentByDate(ctx.session.Class.homework, date);
+					const homework = filterContentByDate(
+						ctx.session.Class.homework,
+						date
+					);
 					if (homework.length === 0) {
-						ctx.reply('На данный день не заданно ни одного задания');
+						ctx.reply("На данный день не заданно ни одного задания");
 						ctx.scene.enter(sceneNames.default);
 					} else {
 						//@ts-ignore
 						const parsedHomework = mapHomeworkByLesson(homework);
 
-						let message = `Задание на ${date.getDate()} ${monthsRP[date.getMonth()]}\n`;
+						let message = `Задание на ${date.getDate()} ${
+							monthsRP[date.getMonth()]
+						}\n`;
 
-						ctx.reply(message, null, await createDefaultKeyboard(undefined, ctx));
+						ctx.reply(
+							message,
+							null,
+							await createDefaultKeyboard(undefined, ctx)
+						);
 
 						sendHomework(parsedHomework, ctx.bot, [ctx.message.user_id]);
 
@@ -229,7 +260,7 @@ const checkHomeworkScene = new Scene(
 			console.error(e);
 			ctx.scene.enter(sceneNames.error);
 		}
-	},
+	}
 );
 
 module.exports = checkHomeworkScene;
